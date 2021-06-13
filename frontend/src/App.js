49e2ccbe520cf7fe7b5c0ebd5253184b0b177284
iamsuigeneris <irewolelanre@gmail.com>
2021-06-13 09:50:05 +0100

    connecting frontend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import LandingPage from './components/screens/landingPage/LandingPage';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import MyNotes from './components/screens/landingPage/myNotes/MyNotes';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello World</h1>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <main>
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/mynotes" component={MyNotes} />
+      
+    </main>
+  <Footer />
+  </Router>
+)
 
+    
 export default App;
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Form, FormControl, Nav, Navbar, NavDropdown, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+      <Navbar bg='primary' expand='lg' variant='dark'>
+        <Container>
+          <Navbar.Brand>
+            <Link to='/'>Note Zipper</Link>
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls='navbarScroll' />
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav className='m-auto'>
+              <Form className='d-flex'>
+                <FormControl
+                  type='search'
+                  placeholder='Search'
+                  className='mr-2'
+                  aria-label='Search'
+                />
+              </Form>
+            </Nav>
+            <Nav>
+              <Nav.Link>
+                <Link to='/mynotes'>My Notes</Link>
+              </Nav.Link>
+
+              <NavDropdown title='Lanre Irewole' id='navbarScrollingDropdown'>
+                <NavDropdown.Item href='#action3'>My Profile</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='#action3'>Logout</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+}
+
+export default Header
